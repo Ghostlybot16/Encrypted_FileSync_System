17b5d9df5734f8e5f6a1c6a756738a7b89025bee
Kramptj KC <kramptj@gmail.com>
2024-10-24 11:06:31 -0400

    Edited comments in FileEncryptor.java
    The comments in the FileEncryptor.java file were edited. Fixed
    typos, capitalized first letter of each comment (eye candy),
    changed the wording to make it sound better.

--- src/FileEncryptor.java
+++ src/FileEncryptor.java
@@ -1,5 +1,3 @@
-import jdk.internal.util.xml.impl.Input;
-
 import java.io.*; // Input/Output classes for file handling
 import javax.crypto.Cipher; // Provides cryptographic encryption and decryption functions
 import javax.crypto.SecretKey; // Secret key used in encryption and decryption
@@ -19,24 +17,24 @@ public class FileEncryptor {
     private static final int ITERATIONS = 100000; // Number of iterations for PBKDF2
     private static final int keyLength = 128; // AES-128 key length
 
-    // Method to generate AES key from a user-input password and a random salt
+    // Method to generate AES key using a user-input password and a random generated salt
     public static SecretKey KeyGenFromPassword(String password, byte[] salt) throws Exception {
 
-        // Key specification/Blueprint used to generate the cryptographic key
-        // Uses the values of user-inputted password, salt, ITERATIONS to generate the AES key
+        // Key specification/blueprint used to generate the cryptographic key
+        // Uses the values from the user-input password, salt, ITERATIONS to generate the AES key
         PBEKeySpec keySpec = new PBEKeySpec(password.toCharArray(), salt, ITERATIONS, keyLength);
 
         /*  SecretKeyFactory provides the functionality to convert key specifications into actual secret keys
         *   which can be used to encrypt/decrypt.
         *   It takes keySpec and uses it to generate a secretKey
         *
-        *   This line says to use PBKDF2 algorithm with HMAC-SHA256 hashing algorithm to generate a unique password */
+        *   Use PBKDF2 algorithm with HMAC-SHA256 hashing algorithm to generate a unique password */
         SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
 
         // Generates secretKey using the keySpec values of password, salt, iterations and key length
         SecretKey secretKey = keyFactory.generateSecret(keySpec);
 
-        // Returns AES-compatible secret key
+        // Returns AES secret key
         return new SecretKeySpec(secretKey.getEncoded(), algo);
     }
 
@@ -63,15 +61,15 @@ public class FileEncryptor {
         // Wrapping the output stream for encryption
         CipherOutputStream encryptedOutputStream = new CipherOutputStream(outputFileStream, encryptionCipher);
 
-        /* dataBuffer is a temporary storage array that can hold upto 1024 bytes = 1KB of data at a time while
+        /* dataBuffer is a temporary storage array that can hold up to 1024 bytes = 1KB of data at a time while
         * reading and writing a file. This value can be changed. */
         byte[] dataBuffer = new byte[1024];
 
-        // variable to hold the number of bytes read
+        // Variable to hold the number of bytes read
         int bytesRead;
 
         /*  inputFileStream.read(dataBuffer) reads up to 1024 bytes of data from the input stream into the buffer array
-        *   If there's data available then the buffer is filled upto 1024 bytes
+        *   If there's data available then the buffer is filled up to 1024 bytes
         *   If it reaches EOF then it returns -1, meaning there's no more data to read hence breaking the while loop */
         while ((bytesRead = inputFileStream.read(dataBuffer)) != -1){
             encryptedOutputStream.write(dataBuffer, 0, bytesRead); // Writes the contents of the buffer to encryptedOutputStream (The encrypted output stream)
@@ -81,14 +79,14 @@ public class FileEncryptor {
     }
 
     // Method to decrypt file
-    // Uses the same logic as encryption but this method runs in decryption mode, hence it decrypts the data
+    // Uses the same logic as encryption but this method runs in decryption mode, therefore it decrypts the data
     public static void decryptFile(InputStream inputFileStream, OutputStream outputFileStream, SecretKey encryptionKey) throws Exception{
         Cipher encryptionCipher = Cipher.getInstance(algo);
 
         // Initialize the cipher in decryption mode. It will use the encryptionKey value to perform the decryption
         encryptionCipher.init(Cipher.DECRYPT_MODE, encryptionKey);
 
-        // Creation of a CipherInputStream to decrypt the data as it is read
+        // Creation of a CipherInputStream to decrypt the data as it reads
         // It wraps the input stream for decryption
         CipherInputStream decryptedInputStream = new CipherInputStream(inputFileStream, encryptionCipher);
 
@@ -99,7 +97,7 @@ public class FileEncryptor {
         *   A buffer array of 1024bytes is created to temporarily store data as it's being read from the input stream
         *   decryptedInputStream.read(dataBuffer) reads encrypted data from the input stream, decrypts it and places the
         *   decrypted data into the buffer.
-        *   If data is avaibale, the decrypted data is filled upto 1024bytes
+        *   If data is available, the decrypted data is filled up to 1024bytes
         *   If it reaches EOF, then it returns -1, thus breaking the while loop*/
         while((bytesRead = decryptedInputStream.read(dataBuffer)) != -1){
             outputFileStream.write(dataBuffer, 0, bytesRead);
